0eef2224a130b2d825b78e3c32a8cbc7369c1f6b
Psychpsyo <psychpsyo@gmail.com>
2022-07-30 09:51:19 +0200

    Initial commit

---
+++ DefaultVoiceMode.cs
@@ -0,0 +1,40 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System.Reflection;
+
+namespace DefaultVoiceMode
+{
+    public class DefaultVoiceMode : NeosMod
+    {
+        public override string Name => "DefaultVoiceMode";
+        public override string Author => "Psychpsyo";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/Psychpsyo/DefaultVoiceMode";
+
+        public static bool setInitialVoiceMode = false;
+
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<VoiceMode> defaultVoiceMode = new ModConfigurationKey<VoiceMode>("Default Voice Mode", "What to set your voice mode to on startup.", () => VoiceMode.Normal);
+
+        private static ModConfiguration config;
+        public override void OnEngineInit() {
+            config = GetConfiguration();
+
+            Harmony harmony = new Harmony("Psychpsyo.DefaultVoiceMode");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(VoiceModeSync), "OnCommonUpdate")]
+        class SetDefaultVoiceModeOnStartup
+        {
+            static void Postfix(VoiceModeSync __instance) {
+                if (!setInitialVoiceMode) {
+                    __instance.FocusedWorldVoiceMode.Value = config.GetValue(defaultVoiceMode);
+                    __instance.GlobalMute.Value = config.GetValue(defaultVoiceMode) == VoiceMode.Mute;
+                    setInitialVoiceMode = true;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# DefaultVoiceMode - The Mod
+
+This is a mod for NeosVR that lets you set your default voice mode for when the game starts up.
